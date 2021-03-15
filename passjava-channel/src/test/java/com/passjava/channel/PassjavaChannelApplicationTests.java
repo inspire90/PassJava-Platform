b82a57fe63cd4aa68734bed0c5f96de9fdbca43f
inspire90 <st13849375638@163.com>
2021-03-15 14:20:30 +0800

    架构结构

---
+++ passjava-channel/src/test/java/com/passjava/channel/PassjavaChannelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.passjava.channel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassjavaChannelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
